8ed685940cfc59338051dc8bf847751b3816b714
Vishal-Kamath <vishalkamath853@gmail.com>
2024-03-31 20:58:40 +0530

    ⬆️update: zindex

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -12,11 +12,11 @@ const AuthLayout: FC<{ children: ReactNode }> = ({ children }) => {
         height={1000}
         className="fixed left-0 top-0 h-full w-full object-cover"
       />
-      <div className="fixed flex h-full min-h-screen flex-col justify-center max-md:hidden">
+      <div className="fixed z-10 flex h-full min-h-screen flex-col justify-center max-md:hidden">
         <Logo className="w-full max-w-[15rem] fill-white" />
         <p className="text-sm font-medium text-fuchsia-300">The purple pill</p>
       </div>
-      <div className="h-24 w-full max-md:mx-auto max-md:max-w-sm">
+      <div className="z-10 h-24 w-full max-md:mx-auto max-md:max-w-sm">
         <Logo className="w-full max-w-[12.5rem] fill-white md:hidden" />
       </div>
       <div className="z-10 flex h-full w-full flex-col items-center py-12 md:min-h-screen md:justify-center md:py-20">
