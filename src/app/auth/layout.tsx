906421340eb7054947a6d37a99b7c00a0479a566
Vishal-Kamath <vishalkamath853@gmail.com>
2024-03-31 17:04:28 +0530

    ➕Add: logo

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -12,7 +12,11 @@ const AuthLayout: FC<{ children: ReactNode }> = ({ children }) => {
         height={1000}
         className="fixed left-0 top-0 -z-10 h-full w-full object-cover"
       />
-      <div className="flex h-full w-full flex-col items-center justify-center"></div>
+      <div className="fixed flex h-full min-h-screen flex-col justify-center">
+        <Logo className="w-full max-w-[15rem] fill-white" />
+        <p className="text-sm font-medium text-fuchsia-300">The purple pill</p>
+      </div>
+      <div className="w-full"></div>
       <div className="flex h-full min-h-screen w-full flex-col items-center justify-center py-20">
         <Suspense>{children}</Suspense>
       </div>
