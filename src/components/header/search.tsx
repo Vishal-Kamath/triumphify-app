55c7eeb7f4437339fafb7c582764ef287f0ea36a
Vishal-Kamath <vishalkamath853@gmail.com>
2024-04-03 17:13:11 +0530

    ⬆️update: search

--- src/components/header/search.tsx
+++ src/components/header/search.tsx
@@ -1,9 +1,12 @@
 import { cn } from "@/lib/utils";
 import { Search } from "lucide-react";
-import { FC, HTMLAttributes, createContext } from "react";
+import { FC, HTMLAttributes } from "react";
 
-interface SearchProps extends HTMLAttributes<HTMLDivElement> {}
-const SearchBar: FC<SearchProps> = ({ className }) => {
+interface SearchProps extends HTMLAttributes<HTMLDivElement> {
+  value: string;
+  onValueChange: (value: string) => void;
+}
+const SearchBar: FC<SearchProps> = ({ className, value, onValueChange }) => {
   return (
     <div
       className={cn(
@@ -11,12 +14,15 @@ const SearchBar: FC<SearchProps> = ({ className }) => {
         className,
       )}
     >
+      <Search className="h-10 w-10 p-2 pl-3" />
+
       <input
         type="search"
-        className="h-full w-full px-3 outline-none"
+        value={value}
+        onChange={(e) => onValueChange(e.target.value)}
+        className="h-full w-full pr-3 outline-none"
         placeholder="Search products..."
       />
-      <Search className="h-10 w-10 p-2 pr-3" />
     </div>
   );
 };
