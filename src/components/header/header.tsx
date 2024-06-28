c79047585dcece88fa4ea718311f8d1ae1998a6b
Vishal-Kamath <vishalkamath853@gmail.com>
2024-06-28 22:09:24 +0530

    ❌remove: searchbar

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,28 +1,17 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { FC, useEffect, useState } from "react";
 import UserSection from "./user-section";
-import Image from "next/image";
-import SearchBar from "./search";
 import CartButton from "./cart-button";
 import { SidebarButton } from "../sidebar/sidebar";
-import { buttonVariants } from "../ui/button";
 import { Menu, X } from "lucide-react";
 import Logo from "../misc/logo";
 
 const Header: FC = () => {
   const pathname = usePathname();
   const isHidden = pathname.startsWith("/auth");
-  const isArtHidden =
-    pathname.startsWith("/auth") ||
-    pathname.startsWith("/account") ||
-    pathname.startsWith("/wishlist");
-
-  const isSearchHidden =
-    pathname.startsWith("/auth") || pathname.startsWith("/tickets/");
 
   const [top, setTop] = useState(true);
 
@@ -36,63 +25,35 @@ const Header: FC = () => {
 
   const [open, setOpen] = useState(false);
 
-  // search
-  const [search, setSearch] = useState("");
-
   return (
     <>
-      <div className="h-[7.25rem] md:h-[4.5rem]"></div>
+      <div className="h-[4.5rem]"></div>
       <div
         className={cn(
-          "padding-x fixed left-0 top-0 isolate z-[995] w-full",
+          "padding-x fixed left-0 top-0 isolate z-[995] h-[4.5rem] w-full",
           isHidden && "hidden",
           "bg-slate-950 text-white",
           !top && "border-b-1 border-slate-700 shadow-sm",
         )}
       >
-        <header className="relative flex flex-col gap-3 py-4">
-          {/* <div
-          className={cn(
-            "absolute left-0 top-0 -z-30 h-[30rem] w-[30rem] -translate-x-1/2 -translate-y-1/2 rounded-full bg-gradient-to-br from-purple-50/25 via-purple-200/25 to-purple-400/75",
-            isArtHidden ? "hidden" : "",
-          )}
-        ></div> */}
-          <div className="flex w-full items-center justify-between gap-3">
-            <div className="flex items-center gap-2">
-              <SidebarButton
-                open={open}
-                onOpenChange={setOpen}
-                className={cn(
-                  "flex h-9 w-9 items-center justify-center rounded-md bg-slate-50 text-slate-600 hover:text-slate-800 md:hidden",
-                )}
-              >
-                {open ? (
-                  <X className="h-6 w-6" />
-                ) : (
-                  <Menu className="h-6 w-6" />
-                )}
-              </SidebarButton>
-              <Logo className="aspect-auto h-8 w-full fill-white object-contain object-left" />
-            </div>
-
-            <SearchBar
-              value={search}
-              onValueChange={setSearch}
-              className="max-md:hidden"
-            />
-            <div className="flex gap-3">
-              <CartButton />
-              <UserSection showRounded={top} />
-            </div>
+        <header className="flex h-full w-full items-center justify-between gap-3">
+          <div className="flex items-center gap-2">
+            <SidebarButton
+              open={open}
+              onOpenChange={setOpen}
+              className={cn(
+                "flex h-9 w-9 items-center justify-center rounded-md bg-slate-50 text-slate-600 hover:text-slate-800 md:hidden",
+              )}
+            >
+              {open ? <X className="h-6 w-6" /> : <Menu className="h-6 w-6" />}
+            </SidebarButton>
+            <Logo className="aspect-auto h-8 w-full fill-white object-contain object-left" />
           </div>
-          <SearchBar
-            value={search}
-            onValueChange={setSearch}
-            className={cn(
-              "max-w-full md:hidden",
-              isSearchHidden ? "hidden" : "",
-            )}
-          />
+
+          <div className="ml-auto flex gap-3">
+            <CartButton />
+            <UserSection showRounded={top} />
+          </div>
         </header>
       </div>
     </>
