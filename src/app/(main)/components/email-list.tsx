97c5c5634569b104a243c3f7a5bd0fcf02410cd0
Vishal-Kamath <vishalkamath853@gmail.com>
2024-07-11 11:47:07 +0530

    ⬆️minor updates

--- src/app/(main)/components/email-list.tsx
+++ src/app/(main)/components/email-list.tsx
@@ -1,26 +1,35 @@
 import { Input } from "@/components/ui/input";
+import { Mail } from "lucide-react";
 import { FC } from "react";
 
 const EmailListContactForm: FC = () => {
   return (
     <div className="py-9">
-      <div className="padding-x flex w-full justify-between bg-purple-950/60 py-9">
-        <div className="flex flex-col gap-2">
-          <h4 className="text-3xl text-slate-200">Join our mailing list</h4>
-          <p className="text-lg text-slate-500">
-            to stay up for everything we have in store for you
-          </p>
+      <div className="flex flex-col bg-purple-600">
+        <div className="h-9 w-full rounded-b-[1.5rem] bg-slate-950"></div>
+        <div className="padding-x flex w-full items-center justify-between gap-8 py-14 max-md:flex-col max-md:items-center">
+          <div className="flex flex-col gap-2 max-md:items-center">
+            <h4 className="text-3xl font-semibold text-white">
+              Join our mailing list
+            </h4>
+            <p className="text-lg font-medium text-purple-300">
+              To stay up for everything we have in store for you.
+            </p>
+          </div>
+          <div className="relative isolate flex w-full max-w-sm gap-3">
+            <Input
+              placeholder=" "
+              className="peer border-slate-200 bg-stone-100/25 text-white"
+            />
+            <span className="absolute left-3 top-1/2 -z-10 hidden -translate-y-1/2 text-slate-100/80 peer-placeholder-shown:flex">
+              Enter your Email
+            </span>
+            <button className="flex aspect-square size-10 flex-shrink-0 items-center justify-center rounded-md bg-slate-200 text-slate-700 hover:bg-white">
+              <Mail className="size-5" />
+            </button>
+          </div>
         </div>
-        <div className="flex w-full flex-col gap-3 md:max-w-sm">
-          <Input
-            placeholder="Enter you name"
-            className="border-gray-600 bg-gray-700/50 focus-visible:ring-white/50"
-          />
-          <Input
-            placeholder="Enter you email"
-            className="border-gray-600 bg-gray-700/50 focus-visible:ring-white/50"
-          />
-        </div>
+        <div className="h-9 w-full rounded-t-[1.5rem] bg-slate-950"></div>
       </div>
     </div>
   );
