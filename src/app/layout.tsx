139c56f624a1e8c10352c945be429302002e365d
Vishal-Kamath <vishalkamath853@gmail.com>
2024-07-11 21:11:15 +0530

    ⬆️update: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,6 +43,28 @@ export const metadata: Metadata = {
     },
   ],
 
+  // Favicons
+  icons: {
+    icon: [
+      { url: "/favicon/favicon.ico" },
+      { url: "/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [{ url: "/favicon/apple-touch-icon.png", type: "image/png" }],
+    other: [
+      {
+        url: "/favicon/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/favicon/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  },
+
   openGraph: {
     title: "Triumphify - Natural Men's Sexual Health Supplement",
     description:
